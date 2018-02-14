a6bdd579949d371465bec23af0558021c512ef2a
Tony Manion <tmanion@fatwallet.com>
2018-02-14 08:56:23 -0600

    added get by description methods for categories and UoM

--- src/main/java/com/tony/recipeapp/controllers/IndexController.java
+++ src/main/java/com/tony/recipeapp/controllers/IndexController.java
@@ -1,5 +1,12 @@
 package com.tony.recipeapp.controllers;
 
+import com.tony.recipeapp.domain.Category;
+import com.tony.recipeapp.domain.UnitOfMeasure;
+import com.tony.recipeapp.repositories.CategoryRepository;
+import com.tony.recipeapp.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +18,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @version  $Revision$, $Date$
  */
 @Controller public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
     /**
      * Creates a new IndexController object.
+     *
+     * @param  categoryRepository
+     * @param  unitOfMeasureRepository
      */
-    public IndexController() {
+    public IndexController(final CategoryRepository categoryRepository,
+        final UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
     /**
@@ -24,6 +40,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
      */
     @RequestMapping({ "", "/", "/index" })
     public String getIndexPage() {
+        final Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        final Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UoM Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/tony/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/tony/recipeapp/repositories/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.tony.recipeapp.repositories;
 
 import com.tony.recipeapp.domain.Category;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -11,4 +13,12 @@ import org.springframework.data.repository.CrudRepository;
  * @version  $Revision$, $Date$
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   description
+     *
+     * @return  Optional
+     */
+    Optional<Category> findByDescription(String description);
 }
